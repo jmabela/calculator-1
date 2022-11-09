77fa4c6e5d4800b26c749441133eb482ab61fe9a
jmabela <josselyn53@gmail.com>
2022-11-08 20:24:11 -0800

    added x+ and cube+

--- arithmetic.py
+++ arithmetic.py
@@ -47,3 +47,14 @@ def mod(num1, num2):
     """Return the remainder of num1 / num2."""
 
     return num1 % num2
+
+def add_mult(num1, num2, num3):
+    """Get sum of num1 and num2 and multiply it by num3"""
+
+    return (num1 + num2) * num3
+
+def add_cubes(num1, num2):
+    """Add the cubes of num1 and num2"""
+
+    return (num1 ** 3) + (num2 **3)
+    
